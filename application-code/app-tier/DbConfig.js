4c7565d7a1196a65bf866d5606634e55bfd3c3a4
Jeevanreddy512 <91076918+Jeevanreddy512@users.noreply.github.com>
2024-06-16 01:21:46 +0530

    Update DbConfig.js dbname

--- application-code/app-tier/DbConfig.js
+++ application-code/app-tier/DbConfig.js
@@ -1,6 +1,6 @@
 module.exports = Object.freeze({
-    DB_HOST : '',
-    DB_USER : '',
-    DB_PWD : '',
-    DB_DATABASE : ''
+    DB_HOST : 'database-1.c7kqomscy22n.us-east-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'admin512',
+    DB_DATABASE : 'webappdb'
 });
